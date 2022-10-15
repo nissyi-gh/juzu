af6ea33ed031f756784885dab12c1cf57fc2a6a0
yuta.onishi <rad6242@icloud.com>
2022-10-15 17:13:34 +0900

    first commit

---
+++ lib/juzu/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Juzu
+  VERSION = "0.1.0"
+end
---
+++ spec/juzu_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Juzu do
+  it "has a version number" do
+    expect(Juzu::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
