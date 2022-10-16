8ce6a490193cb44fbc2425d8e910489529f95dc6
yuta.onishi <rad6242@icloud.com>
2022-10-16 13:48:41 +0900

    juzuからJuzu::Preset.new.execを呼び出せるようにする
    コマンドを実行できるようにします。

--- lib/juzu.rb
+++ lib/juzu.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "juzu/version"
+require_relative "juzu/command"
 
 module Juzu
   class Error < StandardError; end
   # Your code goes here...
 end
+
+Juzu::Preset.new.exec
